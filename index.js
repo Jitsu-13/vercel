994b33fac265df9801636453beb8709852d630ee
Sujit Yadav <sujit.v.yadav@gmail.com>
2023-12-30 00:21:36 +0530

    test

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
-const app = require('./src/app.js');
-const port = process.env.PORT || 8001;
+// const app = require('./src/app.js');
+// const port = process.env.PORT || 8001;
 
-// Server
-app.listen(port, () => {
-   console.log(`Listening on: http://localhost:${port}`);
+const express = require('express')
+const app = express()
+const PORT = 4000
+
+
+app.get('/home', (req, res) => {
+  res.status(200).json('Welcome, your app is working well');
+})
+
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`);
 });
+
+// Export the Express API
+module.exports = app
+
+// // Server
+// app.listen(port, () => {
+//    console.log(`Listening on: http://localhost:${port}`);
+// });
