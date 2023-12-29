94f62988876f38565f5b337dd495e915272f234f
Sujit Yadav <sujit.v.yadav@gmail.com>
2023-12-30 01:27:24 +0530

    test

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-const express = require('express')
-const app = express()
-const PORT = 4000
+// const express = require('express')
+// const app = express()
+// const PORT = 4000
+// var AppController = require('./src/controllers/AppController');
+// const { check } = require('express-validator');
+// const jwt = require('jsonwebtoken');
+// const JWT_SECRET = process.env.JWT_SECRET;
+
+// app.post('/app/login-employee', [
+//    check('mobile').trim().isLength({ min: 1 }).withMessage('Enter mobile number'),
+//    check('password').trim().isLength({ min: 1 }).withMessage('Enter password')
+// ], AppController.get_employee)
+
+// // Server
+// app.listen(port, () => {
+//    console.log(`Listening on: http://localhost:${port}`);
+// });
+
+//  // Export the Express API
+//  module.exports = app
+const express = require('express');
+const app = express();
+const PORT = 4000;
 var AppController = require('./src/controllers/AppController');
 const { check } = require('express-validator');
 const jwt = require('jsonwebtoken');
@@ -9,12 +29,12 @@ const JWT_SECRET = process.env.JWT_SECRET;
 app.post('/app/login-employee', [
    check('mobile').trim().isLength({ min: 1 }).withMessage('Enter mobile number'),
    check('password').trim().isLength({ min: 1 }).withMessage('Enter password')
-], AppController.get_employee)
+], AppController.get_employee);
 
 // Server
-app.listen(port, () => {
-   console.log(`Listening on: http://localhost:${port}`);
+app.listen(PORT, () => {
+   console.log(`Listening on: http://localhost:${PORT}`);
 });
 
- // Export the Express API
- module.exports = app
+// Export the Express API
+module.exports = app;
