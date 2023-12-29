3076884aa7ad713a8e558063dc7691b31dbf3a3c
Sujit Yadav <sujit.v.yadav@gmail.com>
2023-12-30 00:55:15 +0530

    test

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const app = require('./src/app.js');
 const port = process.env.PORT || 8001;
 
+app.get('/home', (req, res) => {
+   res.status(200).json('Welcome, your app is working well');
+ })
+
 // Server
 app.listen(port, () => {
    console.log(`Listening on: http://localhost:${port}`);
