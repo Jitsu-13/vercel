00ea054c8fe4793c6de76ebb8563098d913cb097
Sujit Yadav <sujit.v.yadav@gmail.com>
2023-12-30 01:25:02 +0530

    test

--- index.js
+++ index.js
@@ -1,15 +1,20 @@
-// const app = require('./src/app.js');
-// const port = process.env.PORT || 8001;
+const express = require('express')
+const app = express()
+const PORT = 4000
+var AppController = require('./src/controllers/AppController');
+const { check } = require('express-validator');
+const jwt = require('jsonwebtoken');
+const JWT_SECRET = process.env.JWT_SECRET;
 
-// // Server
-// app.listen(port, () => {
-//    console.log(`Listening on: http://localhost:${port}`);
-// });
-
-const app = require('./src/app.js');
-const port = process.env.PORT || 8001;
+app.post('/app/login-employee', [
+   check('mobile').trim().isLength({ min: 1 }).withMessage('Enter mobile number'),
+   check('password').trim().isLength({ min: 1 }).withMessage('Enter password')
+], AppController.get_employee)
 
 // Server
 app.listen(port, () => {
-  console.log(`Listening on: http://localhost:${port}`);
+   console.log(`Listening on: http://localhost:${port}`);
 });
+
+ // Export the Express API
+ module.exports = app
--- src/app.js
+++ src/app.js
@@ -1,77 +1,35 @@
-// const express       = require('express');
-// var bodyParser      = require('body-parser');
-// var cors            = require('cors');
-// var dotenv          = require('dotenv');
-// var MongoClient     = require('mongoose');
-
-// const app = express();
-// app.use(cors());
-
-// // Enable environment variable
-// dotenv.config();
-
-// // Connection to MongoDB server 
-// MongoClient.connect(process.env.MONGO_URI,{useNewUrlParser:true, useUnifiedTopology: true},function(){
-//     console.log('Connect to MongoDB');
-// });
-
-// MongoClient.set('useFindAndModify', false);
-// MongoClient.set('useCreateIndex', true);
-
-// app.use(bodyParser.urlencoded({
-//     extended:true,
-//     limit: '10mb',
-//     parameterLimit: 100000
-// }));
-// app.use(bodyParser.json()); // take json data
-
-// app.use(express.static('public'));
-// app.use('/',express.static('public'));
-
-// process.env.TZ = 'Asia/Kolkata'; // here is the magical line
-
-// require('./router/admin.js')(app);
-// require('./router/app.js')(app);
-// module.exports = app;
-
-
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const dotenv = require('dotenv');
-const mongoose = require('mongoose');
+const express       = require('express');
+var bodyParser      = require('body-parser');
+var cors            = require('cors');
+var dotenv          = require('dotenv');
+var MongoClient     = require('mongoose');
 
 const app = express();
 app.use(cors());
 
+// Enable environment variable
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  useFindAndModify: false,
-  useCreateIndex: true
-})
-  .then(() => {
-    console.log('Connected to MongoDB');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB:', error.message);
-  });
+// Connection to MongoDB server
+MongoClient.connect(process.env.MONGO_URI,{useNewUrlParser:true, useUnifiedTopology: true},function(){
+    console.log('Connect to MongoDB');
+});
+
+MongoClient.set('useFindAndModify', false);
+MongoClient.set('useCreateIndex', true);
 
 app.use(bodyParser.urlencoded({
-  extended: true,
-  limit: '10mb',
-  parameterLimit: 100000
+    extended:true,
+    limit: '10mb',
+    parameterLimit: 100000
 }));
-app.use(bodyParser.json());
+app.use(bodyParser.json()); // take json data
 
 app.use(express.static('public'));
-app.use('/', express.static('public'));
+app.use('/',express.static('public'));
 
-process.env.TZ = 'Asia/Kolkata';
+process.env.TZ = 'Asia/Kolkata'; // here is the magical line
 
 require('./router/admin.js')(app);
 require('./router/app.js')(app);
-
 module.exports = app;
